5268118a5c5702f8f0cb633018593fecb87a0c4a
Dab <stevandabraham@gmail.com>
2025-04-07 17:42:46 +0300

    files added

---
+++ script/data.js
@@ -0,0 +1,71 @@
+const products = {
+    'c1': {
+        name: 'The New Earth Tan Penny Loafers',
+        price: 4000,
+        features: [
+            'High grain leather in deep earth brown', 
+            'Made in Kenya', 
+            'Custom sizing',
+            'Calf-skin lining',
+            'Strong rubber soles'
+        ],
+        colors: [
+            { name: 'Earth Tan', code: '#9b4a1b' },
+            { name: 'Black', code: '#000000' },
+            { name: 'Brown', code: '#8B4513' }
+        ],
+        sizes: ['7', '8', '9', '10', '11']
+    },
+    'c2': {
+        name: 'The New Black Brogues',
+        price: 4000,
+        features: [
+            'Black high grain leather', 
+            'Made in Kenya', 
+            'Custom sizing',
+            'Leather lining',
+            'Strong rubber soles'
+        ],
+        colors: [
+            { name: 'Earth Tan', code: '#9b4a1b' },
+            { name: 'Black', code: '#000000' },
+            { name: 'Brown', code: '#8B4513' }
+        ],
+        sizes: ['7', '8', '9', '10', '11']
+    },
+    'c3': {
+        name: 'The New Dark Tan Brogues',
+        price: 4000,
+        features: [
+            'Dark tan high grain leather', 
+            'Made in Kenya', 
+            'Custom sizing',
+            'Leather lining',
+            'Strong rubber soles'
+        ],
+        colors: [
+            { name: 'Earth Tan', code: '#9b4a1b' },
+            { name: 'Black', code: '#000000' },
+            { name: 'Brown', code: '#8B4513' }
+        ],
+        sizes: ['7', '8', '9', '10', '11']
+    },
+    'c4': {
+        name: 'The New Dark Tan Oxfords',
+        price: 4000,
+        features: [
+            'Dark tan high grain leather', 
+            'Made in Kenya', 
+            'Custom sizing',
+            'Leather lining',
+            'Strong rubber soles'
+        ],
+        colors: [
+            { name: 'Earth Tan', code: '#9b4a1b' },
+            { name: 'Black', code: '#000000' },
+            { name: 'Brown', code: '#8B4513' }
+        ],
+        sizes: ['7', '8', '9', '10', '11']
+    },
+    // Add more products as needed
+};
